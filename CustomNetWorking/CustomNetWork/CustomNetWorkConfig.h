29f8e63d57e91bf2de9305988d2ce6b0b5c479ae
Singularity_Lee <496736912@qq.com>
2020-07-28 23:06:24 +0800

    头文件quote引用

--- CustomNetWorking/CustomNetWork/CustomNetWorkConfig.h
+++ CustomNetWorking/CustomNetWork/CustomNetWorkConfig.h
@@ -7,7 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "AFNetworking.h"
+#import <AFNetworking/AFNetworking.h>
 
 /** 数据请求格式 */
 typedef NS_ENUM(NSUInteger, CustomRequestSerializer) {
--- CustomNetWorking/CustomNetWork/CustomNetWorkManager.h
+++ CustomNetWorking/CustomNetWork/CustomNetWorkManager.h
@@ -7,7 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "AFNetworking.h"
+#import <AFNetworking/AFNetworking.h>
 #import "CustomNetWorkConfig.h"
 
 NS_ASSUME_NONNULL_BEGIN
